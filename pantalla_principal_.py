263326c71b278aa130f73d1e2e61675b4a5d70ae
celia <celitavillalta@gamil.com>
2026-06-30 23:36:18 -0600

    Se agregó custom tkinter.

--- pantalla_principal_.py
+++ pantalla_principal_.py
@@ -1,31 +1,61 @@
-import tkinter as tk 
-from tkinter import *
+
+import customtkinter as ctk
 from pantalla_registro_de_produc import registrar_producto 
  
 
 #tamaños de la pantalla en general. 
 
-screen = tk.Tk()
+screen = ctk.CTk()
 screen.title("Sistema de inventario.")
-screen.geometry("700x500")
-screen.resizable(True, True)
+screen.geometry("400x500")
+screen.resizable(False, False)
 
 #Labels. 
 
-label_principal = tk.Label(screen, text= "INVENTARIO", font= ("Helvetica", 25, "bold"), bg= "#E0E0E0") 
-label_principal.pack(pady= 20) 
+label_principal = ctk.CTkLabel(
+    master = screen, 
+    text= "INVENTARIO", 
+    font= ("Helvetica", 25, "bold"),
+    bg_color= "transparent"
+    ) 
+label_principal.pack(pady= 40) 
 
-screen.config(bg = "#E0E0E0") 
+screen.configure(bg_color = "#E0E0E0") 
 
 #Botones 
 
-boton_1= tk.Button(screen, text = "Ingresar Producto", width=20, height=2, font= ("Times New Roman", 15), bg= "#00BFFE", fg= "Black", command = registrar_producto)
+boton_1= ctk.CTkButton(
+    master=screen, 
+    text = "Ingresar Producto",
+    font= ("Times New Roman", 15),
+    bg_color= "#00BFFE",
+    text_color= "Black",
+    width= 250,
+    height= 40,
+    command = registrar_producto
+    )
 boton_1.pack(pady=20)
 
-boton_2= tk.Button(screen, text = "Mostrar Inventario", width=20, height=2, font= ("Times New Roman", 15), bg= "#00BFFE", fg= "Black")
+boton_2= ctk.CTkButton(
+    master=screen, 
+    text = "Mostrar Inventario", 
+    font= ("Times New Roman", 15), 
+    bg_color= "#00BFFE",
+    width= 250,
+    height= 40,
+    text_color= "Black"
+    )
 boton_2.pack(pady=20)  
 
-boton_3 = tk.Button(screen, text = "Eliminar Productos", width=20, height=2, font = ("Times New Roman", 15), bg = "#00BFFE", fg = "Black")
+boton_3 = ctk.CTkButton(
+    master=screen, 
+    text = "Eliminar Productos", 
+    font = ("Times New Roman", 15), 
+    bg_color = "#00BFFE", 
+    text_color = "Black",
+    width = 250,
+    height = 40
+    )
 boton_3.pack(pady= 20) 
 
 
--- pantalla_registro_de_produc.py
+++ pantalla_registro_de_produc.py
@@ -1,56 +1,60 @@
 
-import tkinter as tk
-from tkinter import *
+
+import customtkinter as ctk
 
 
 def registrar_producto():
     # Pantalla secundaria para registrar productos.
-    registrar_ventana = tk.Toplevel() #Sirve para abrir una ventana scundaria sin cerrar la principal.
+    registrar_ventana = ctk.CTkToplevel() #Sirve para abrir una ventana scundaria sin cerrar la principal.
     registrar_ventana.title("Registrar Producto")
-    registrar_ventana.geometry("500x450")
+    registrar_ventana.geometry("500x550")
     registrar_ventana.resizable(False, False)
 
     # labels e inputs.
     
     #Cóigo del producto.
-    label_código = tk.Label(registrar_ventana, text = "Ingrese el código del producto: ", bg="#E0E0E0")
+    label_código = ctk.CTkLabel(registrar_ventana, text = "Ingrese el código del producto: ", bg_color="transparent")
     label_código.pack(pady=10) 
-    entry_código = tk.Entry(registrar_ventana)
+    entry_código = ctk.CTkEntry(registrar_ventana)
     entry_código.pack(pady=5) 
     
     #Categoría del producto (opción múltiple)
-    label_categoría = tk.Label(registrar_ventana, text = "Seleccione una categoría: ", bg="#E0E0E0")
+    label_categoría = ctk.CTkLabel(registrar_ventana, text = "Seleccione una categoría: ", bg_color="transparent")
     label_categoría.pack(pady=10)
     
-    opcion = tk.IntVar()
+    opcion = ctk.IntVar()
     opcion.set(0)
     
-    cb1 = tk.Radiobutton(registrar_ventana, text = "Repuestos", variable=opcion, value=1, bg="#E0E0E0")
+    cb1 = ctk.CTkRadioButton(registrar_ventana, text = "Repuestos", variable=opcion, value=1, bg_color="transparent")
     cb1.pack(padx=10) 
     
-    cb2 = tk.Radiobutton(registrar_ventana, text = "Tecnología", variable=opcion, value= 2, bg="#E0E0E0")
+    cb2 = ctk.CTkRadioButton(registrar_ventana, text = "Tecnología", variable=opcion, value= 2, bg_color="transparent")
     cb2.pack(padx=10)
     
-    cb3 = tk.Radiobutton(registrar_ventana, text="Otro", variable =opcion, value= 3, bg="#E0E0E0")
+    cb3 = ctk.CTkRadioButton(registrar_ventana, text="Otro", variable =opcion, value= 3, bg_color="transparent")
     cb3.pack(padx=10)
     
     
     #Nombre del prooducto.
-    label_nombre = tk.Label(registrar_ventana, text="Ingrese el nombre del producto: ", bg="#E0E0E0")
+    label_nombre = ctk.CTkLabel(registrar_ventana, text="Ingrese el nombre del producto: ", bg_color="transparent")
     label_nombre.pack(pady=10)
-    entry_nombre = tk.Entry(registrar_ventana)
+    entry_nombre = ctk.CTkEntry(registrar_ventana)
     entry_nombre.pack(pady=5)
 
     #Cantidad en existencia del producto. 
-    label_cantidad = tk.Label(registrar_ventana, text="Ingrese la cantidad en existencia del producto: ", bg="#E0E0E0")
+    label_cantidad = ctk.CTkLabel(registrar_ventana, text="Ingrese la cantidad en existencia del producto: ", bg_color="transparent")
     label_cantidad.pack(pady=10)
-    entry_cantidad = tk.Entry(registrar_ventana)
+    entry_cantidad = ctk.CTkEntry(registrar_ventana)
     entry_cantidad.pack(pady=5)
     
     #Precio del producto.
-    label_precio = tk.Label(registrar_ventana, text="Ingrese el precio del producto: ", bg="#E0E0E0")
+    label_precio = ctk.CTkLabel(registrar_ventana, text="Ingrese el precio del producto: ", bg_color="transparent")
     label_precio.pack(pady=10)
-    entry_precio = tk.Entry(registrar_ventana)
+    entry_precio = ctk.CTkEntry(registrar_ventana)
     entry_precio.pack(pady=5)
+    
+    #boton de aceptar.
+    boton_aceptar = ctk.CTkButton(master = registrar_ventana, text="Aceptar", bg_color="#00BFFE", text_color="Black", width= 250, height= 40)
+    boton_aceptar.pack(pady=20)
     
     registrar_ventana.configure(bg= "#E0E0E0")
